4616366e656b7d8af2eefda13a76aceabda489fd
Khumo Mogorosi <khumomogorosi@gmail.com>
2025-07-25 17:31:25 +0200

    feat: implement move field

--- src/app/components/canvas/canvas.ts
+++ src/app/components/canvas/canvas.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { lucideCirclePlus } from '@ng-icons/lucide';
+import { BrnToggleGroupComponent, BrnToggleGroupItemDirective } from '@spartan-ng/brain/toggle-group';
+import { HlmButtonDirective } from '@spartan-ng/helm/button';
+import { HlmIconDirective } from '@spartan-ng/helm/icon';
+import { HlmToggleGroupDirective, HlmToggleGroupItemDirective } from '@spartan-ng/helm/toggle-group';
 
+import { FormService } from '../../services/form-service';
 import { FormEditor } from './form-editor/form-editor';
+import { FormPreview } from './form-preview/form-preview';
 
 @Component({
   selector: 'app-canvas',
-  imports: [FormEditor],
+  imports: [
+    FormEditor,
+    FormPreview,
+    HlmToggleGroupItemDirective,
+    HlmToggleGroupDirective,
+    BrnToggleGroupItemDirective,
+    BrnToggleGroupComponent,
+    HlmButtonDirective,
+    NgIcon,
+    HlmIconDirective,
+  ],
+  providers: [provideIcons({ lucideCirclePlus })],
   templateUrl: './canvas.html',
-  styleUrl: './canvas.scss'
+  styleUrl: './canvas.scss',
 })
 export class Canvas {
+  formService = inject(FormService);
 
+  activeTab = signal<'preview' | 'editor'>('editor');
 }
--- src/app/components/canvas/form-editor/form-editor.ts
+++ src/app/components/canvas/form-editor/form-editor.ts
@@ -1,5 +1,9 @@
 import { CdkDragDrop, DragDropModule } from '@angular/cdk/drag-drop';
 import { Component, inject } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { lucideTrash } from '@ng-icons/lucide';
+import { HlmButtonDirective } from '@spartan-ng/helm/button';
+import { HlmIconDirective } from '@spartan-ng/helm/icon';
 
 import { FieldTypeDefinition, FormField } from '../../../models/field';
 import { FormService } from '../../../services/form-service';
@@ -7,7 +11,14 @@ import { FormFieldComponent } from '../form-field/form-field';
 
 @Component({
   selector: 'app-form-editor',
-  imports: [DragDropModule, FormFieldComponent],
+  imports: [
+    DragDropModule,
+    FormFieldComponent,
+    HlmButtonDirective,
+    NgIcon,
+    HlmIconDirective,
+  ],
+  providers: [provideIcons({ lucideTrash })],
   templateUrl: './form-editor.html',
   styleUrl: './form-editor.scss',
 })
@@ -28,5 +39,15 @@ export class FormEditor {
 
       return;
     }
+
+    const dragData = event.item.data as FormField;
+    const previousRowId = event.previousContainer.data as string;
+
+    this.formService.moveField(
+      dragData.id,
+      previousRowId,
+      rowId,
+      event.currentIndex
+    );
   }
 }
